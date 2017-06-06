e78fc20b0b055bd19fe5fd2d23334e54aea8d4d4
tom <419187544@qq.com>
2017-06-06 21:01:43 +0800

    *)初始化项目

--- Infant.Education/Infant.Education.Common/Enum.cs
+++ Infant.Education/Infant.Education.Common/Enum.cs
@@ -2,11 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Infant.Education.Common
 {
-    public class Class1
-    {
-    }
+
 }
