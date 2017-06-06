cc6cad59d69ab5c71da4589a2b3b725bbdf21d77
tom <419187544@qq.com>
2017-06-06 20:41:16 +0800

    *)项目创建

---
+++ Infant.Education/Infant.Education/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Infant.Education.Startup))]
+namespace Infant.Education
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
